50a4d5b46df3a035bba7600e710dbb22ee8f4a28
iqbalatma <iqbalatma@users.noreply.github.com>
2021-08-18 00:05:56 +0700

    UsersAPI selesai

--- application/controllers/RestfullAPI/LocationAPI.php
+++ application/controllers/RestfullAPI/LocationAPI.php
@@ -5,12 +5,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
 class LocationAPI extends REST_Controller
 {
+    /**
+     * Ini adalah consturctor
+     */
     public function __construct()
     {
         parent::__construct();
         $this->load->model("LocationModel");
     }
 
+
+    /**
+     * Ini method http request
+     */
     public function index_post()
     {
         // data dari method post, dimasukkan dalam variabel input dengan masing-masing valuanya kemudian di insert kedalam database
