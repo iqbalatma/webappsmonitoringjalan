1f2eb6c4152e42b0cb8fcaaefa1cbfcdc873415e
iqbalatma <iqbalatma@users.noreply.github.com>
2021-08-19 01:59:25 +0700

    buntu

--- application/views/vMapsMobileDeteksiJalanRusak.php
+++ application/views/vMapsMobileDeteksiJalanRusak.php
@@ -34,6 +34,68 @@
 <script type="text/javascript">
     $("#alert-jarak").hide();
     var object_devicelocation = new DevicelocationClass();
+
+
+    function get_position(position) {
+        object_devicelocation.latdevice = position.coords.longitude;
+        object_devicelocation.longdevice = position.coords.longitude;
+        object_devicelocation.accuracy = position.coords.accuracy;
+
+        if (object_devicelocation.markerUser) {
+            object_leaflet.map.removeLayer(object_devicelocation.marker_user)
+        }
+        if (object_devicelocation.circle) {
+            object_leaflet.map.removeLayer(object_devicelocation.circle)
+        }
+
+        $("#alert-jarak").hide();
+
+        object_devicelocation.marker_user = L.marker([object_devicelocation.latdevice, object_devicelocation.longdevice], {
+            icon: object_leaflet.user_device_location
+        });
+        object_devicelocation.circle = L.circle([object_devicelocation.latdevice, object_devicelocation.longdevice], {
+            radius: 20
+        });
+
+        object_devicelocation.feature_group = L.featureGroup([object_devicelocation.marker_user, object_devicelocation.circle]).addTo(object_leaflet.map);
+
+        // console.log("ho")
+        if (titikJalanRusakFinal.length == 0) {
+            // ketika user belum menentukan titik rute maka titik jalan rusak yang dilalui akan kosong
+            console.log("Titik jalan rusak tidak ada")
+        } else {
+            // Ketika rute ditemukan dan terdapat jalan rusak pada rute tersebut
+            //cari dulu jarak terpendek dari titik user baru tampilkan alert
+            var index;
+            for (let i = 0; i < titikJalanRusakFinal.length; i++) {
+                jarak = object_devicelocation.distance(titikJalanRusakFinal[i][0], titikJalanRusakFinal[i][1], latdevice, longdevice, "M");
+                if (object_devicelocation.jarak_terpendek == false) { //berarti jarak terpendek belum di set
+                    object_devicelocation.jarak_terpendek = jarak
+                    index = i;
+                } else { //kalau jarak terpendek sudah diset, maka selanjutnya adalah membandingkan jarak di variabel dengan di looping
+                    if (object_devicelocation.jarak_terpendek > jarak) { //jika jarak terpendek lebih besar dari jarak, berarti jarak terbaru tersebut merupakan jarak lebih pendek, simpan ke variabel
+                        object_devicelocation.jarak_terpendek = jarak
+                        index = i;
+                    }
+                }
+            }
+
+
+            console.log(object_devicelocation.jarak_terpendek);
+
+
+            if (object_devicelocation.jarak_terpendek < 100) {
+                $("#alert-jarak").html("Hati-hati ! " + parseFloat(object_devicelocation.jarak_terpendek).toFixed(2) + " m ada jalan berlubang");
+                $("#alert-jarak").show();
+                if (object_devicelocation.jarak_terpendek < 5) {
+                    console.log("pemotongan tereksekusi");
+                    titikJalanRusakFinal.splice(index, 1)
+                }
+            }
+            object_devicelocation.jarak_terpendek = false;
+        }
+
+    }
 </script>
 
 
@@ -42,14 +104,33 @@
     // mengambil data jalan rusak dari database dengan ajax
     var koordinatejalanrusak;
     var ajax = new XMLHttpRequest();
-    ajax.open("OPEN", main_url + "assets/AjaxServer/Serv.php", true);
+    ajax.open("OPEN", object_leaflet.main_url + "assets/AjaxServer/Serv.php", true);
     ajax.send();
     ajax.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            koordinatejalanrusakjson = this.responseText
+            koordinatejalanrusak = this.responseText
+            console.log(koordinatejalanrusak);
         }
     };
 
+    x = [{
+        "latitude": "1.06320954",
+        "longitude": "108.97321338"
+    }, {
+        "latitude": "1.0632212",
+        "longitude": "108.97307615"
+    }, {
+        "latitude": "1.06321072",
+        "longitude": "108.97322186"
+    }, {
+        "latitude": "1.06319638",
+        "longitude": "108.97322468"
+    }, {
+        "latitude": "1.1762523",
+        "longitude": "108.97452799999999"
+    }];
+
+    var object_routingmachine = new RoutingmachineClass(x);
 
 
 
@@ -58,7 +139,22 @@
 
     var demo = [];
 
-    var object_routingmachine = new RoutingmachineClass();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     object_leaflet.map.on('click', function(e) {
--- assets/js/DevicelocationClass.js
+++ assets/js/DevicelocationClass.js
@@ -31,72 +31,74 @@ class DevicelocationClass{
             console.log("Browser tidak support");
         } else {
             setInterval(() => {
-                navigator.geolocation.watchPosition(this.get_position(), this.geoerror(), this.get_options);
+                navigator.geolocation.watchPosition(get_position, this.geoerror, this.get_options);
             }, 1000);
         }
     }
 
-    get_position(position){
-        console.log(position)
-            // this.latdevice = position.coords.latitude
-            // this.longdevice = position.coords.longitude
-            // console.log(this.latdevice);
-            // this.accuracy = position.coords.accuracy
-            // if (this.markerUser) {
-            //     object_leaflet.map.removeLayer(this.marker_user)
-            // }
-            // if (this.circle) {
-            //     object_leaflet.map.removeLayer(this.circle)
-            // }
+   
 
-            // $("#alert-jarak").hide();
-    
-            // this.marker_user = L.marker([this.latdevice, this.longdevice], {
-            //     icon: object_leaflet.user_device_location
-            // });
-            // this.circle = L.circle([this.latdevice, this.longdevice], {
-            //     radius: 20
-            // });
-    
-            // this.feature_group = L.featureGroup([this.marker_user, this.circle]).addTo(object_leaflet.map);
-    
-            // // console.log("ho")
-            // if (titikJalanRusakFinal.length == 0) {
-            //     // ketika user belum menentukan titik rute maka titik jalan rusak yang dilalui akan kosong
-            //     console.log("Titik jalan rusak tidak ada")
-            // } else {
-            //     // Ketika rute ditemukan dan terdapat jalan rusak pada rute tersebut
-            //     //cari dulu jarak terpendek dari titik user baru tampilkan alert
-            //     var index;
-            //     for (let i = 0; i < titikJalanRusakFinal.length; i++) {
-            //         jarak = this.distance(titikJalanRusakFinal[i][0], titikJalanRusakFinal[i][1], latdevice, longdevice, "M");
-            //         if (this.jarak_terpendek == false) { //berarti jarak terpendek belum di set
-            //             this.jarak_terpendek = jarak
-            //             index = i;
-            //         } else { //kalau jarak terpendek sudah diset, maka selanjutnya adalah membandingkan jarak di variabel dengan di looping
-            //             if (this.jarak_terpendek > jarak) { //jika jarak terpendek lebih besar dari jarak, berarti jarak terbaru tersebut merupakan jarak lebih pendek, simpan ke variabel
-            //                 this.jarak_terpendek = jarak
-            //                 index = i;
-            //             }
-            //         }
-            //     }
-    
-    
-            //     console.log(this.jarak_terpendek);
-    
-    
-            //     if (this.jarak_terpendek < 100) {
-            //         $("#alert-jarak").html("Hati-hati ! " + parseFloat(this.jarak_terpendek).toFixed(2) + " m ada jalan berlubang");
-            //         $("#alert-jarak").show();
-            //         if (this.jarak_terpendek < 5) {
-            //             console.log("pemotongan tereksekusi");
-            //             titikJalanRusakFinal.splice(index, 1)
-            //         }
-            //     }
-            //     this.jarak_terpendek = false;
-            // }
+    // get_position(position){
         
-    }
+    //     console.log(position);
+    //         // this.latdevice=22;
+    //         this.longdevice = position.coords.longitude;
+    //         // this.accuracy = position.coords.accuracy;
+    //         if (this.markerUser) {
+    //             object_leaflet.map.removeLayer(this.marker_user)
+    //         }
+    //         if (this.circle) {
+    //             object_leaflet.map.removeLayer(this.circle)
+    //         }
+
+    //         $("#alert-jarak").hide();
+    
+    //         this.marker_user = L.marker([this.latdevice, this.longdevice], {
+    //             icon: object_leaflet.user_device_location
+    //         });
+    //         this.circle = L.circle([this.latdevice, this.longdevice], {
+    //             radius: 20
+    //         });
+    
+    //         this.feature_group = L.featureGroup([this.marker_user, this.circle]).addTo(object_leaflet.map);
+    
+    //         // console.log("ho")
+    //         if (titikJalanRusakFinal.length == 0) {
+    //             // ketika user belum menentukan titik rute maka titik jalan rusak yang dilalui akan kosong
+    //             console.log("Titik jalan rusak tidak ada")
+    //         } else {
+    //             // Ketika rute ditemukan dan terdapat jalan rusak pada rute tersebut
+    //             //cari dulu jarak terpendek dari titik user baru tampilkan alert
+    //             var index;
+    //             for (let i = 0; i < titikJalanRusakFinal.length; i++) {
+    //                 jarak = this.distance(titikJalanRusakFinal[i][0], titikJalanRusakFinal[i][1], latdevice, longdevice, "M");
+    //                 if (this.jarak_terpendek == false) { //berarti jarak terpendek belum di set
+    //                     this.jarak_terpendek = jarak
+    //                     index = i;
+    //                 } else { //kalau jarak terpendek sudah diset, maka selanjutnya adalah membandingkan jarak di variabel dengan di looping
+    //                     if (this.jarak_terpendek > jarak) { //jika jarak terpendek lebih besar dari jarak, berarti jarak terbaru tersebut merupakan jarak lebih pendek, simpan ke variabel
+    //                         this.jarak_terpendek = jarak
+    //                         index = i;
+    //                     }
+    //                 }
+    //             }
+    
+    
+    //             console.log(this.jarak_terpendek);
+    
+    
+    //             if (this.jarak_terpendek < 100) {
+    //                 $("#alert-jarak").html("Hati-hati ! " + parseFloat(this.jarak_terpendek).toFixed(2) + " m ada jalan berlubang");
+    //                 $("#alert-jarak").show();
+    //                 if (this.jarak_terpendek < 5) {
+    //                     console.log("pemotongan tereksekusi");
+    //                     titikJalanRusakFinal.splice(index, 1)
+    //                 }
+    //             }
+    //             this.jarak_terpendek = false;
+    //         }
+        
+    // }
 
 
     
