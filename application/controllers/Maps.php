44923672c7a999b738580d906362b39b4a744a32
iqbalatma <iqbalatma@users.noreply.github.com>
2021-08-18 13:24:21 +0700

    config dinamis berdasarkan env

--- application/controllers/Maps.php
+++ application/controllers/Maps.php
@@ -3,22 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Maps extends CI_Controller
 {
-
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     *	- or -
-     * 		http://example.com/index.php/welcome/index
-     *	- or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
-     */
     public function __construct()
     {
         parent::__construct();
@@ -247,10 +231,9 @@ class Maps extends CI_Controller
             if ($query) {
                 $this->session->set_flashdata('msg', "<div class='alert alert-success fixed-top' role='alert'>$messageFlash</div>");
                 redirect('Maps/verifikasijalan/' . $token . "/true");
-            }else{
-              $this->session->set_flashdata('msg', "<div class='alert alert-danger fixed-top' role='alert'>Status dan verifikasi jalan gagal diperbaharui</div>");
+            } else {
+                $this->session->set_flashdata('msg', "<div class='alert alert-danger fixed-top' role='alert'>Status dan verifikasi jalan gagal diperbaharui</div>");
             }
-
         } else {
             redirect("Auth");
         }
