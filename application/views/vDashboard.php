754ab5b0a7a7695a37f857de9a2aff23f453b824
iqbalatma <35129050+iqbalatma@users.noreply.github.com>
2021-08-25 17:04:32 +0700

    coba suara2

--- application/views/vDashboard.php
+++ application/views/vDashboard.php
@@ -2,6 +2,7 @@
 	<div class="col-xl-12 col-md-6 mb-4">
 		<div class="card border-left-primary shadow h-100 py-2">
 			<div class="card-body">
+				<button onclick="playSound('http://monitoringjalansambas.my.id/assets/tes.mp3')">Play</button>
 				<a href="<?= base_url(); ?>Dashboard/"><button type="button" class="mb-3 btn btn-primary">Data Jalan</button></a>
 				<a href="<?= base_url(); ?>Dashboard/index/1"><button type="button" class="mb-3 btn btn-primary">Data Jalan Rusak</button></a>
 				<a href="<?= base_url(); ?>Dashboard/index/2"><button type="button" class="mb-3 btn btn-primary">Data Jalan Rusak Terverifikasi</button></a>
@@ -77,4 +78,3 @@
 		</div>
 	</div>
 </div>
-<button onclick="playSound('http://monitoringjalansambas.my.id/assets/tes.mp3')">Play</button>
